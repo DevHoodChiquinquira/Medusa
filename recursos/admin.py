aa4a65a819aeb3ba75de7e0827c28f0315428d90
Rafael Romario <romarovin11@gmail.com>
2018-06-23 21:24:48 -0500

    Creacion de modelos de reservacion de lugar y comentarios

--- recursos/admin.py
+++ recursos/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Recursos
+from .models import Recursos, ReservacionLugar, ComentarioReservacionLugar
 # Register your models here.
 @admin.register(Recursos)
 class AdminRecursos(admin.ModelAdmin):
     list_display = ('descripcion', 'recursoTecnologico',)
-    
+
+@admin.register(ReservacionLugar)
+class AdminReservacionLugar(admin.ModelAdmin):
+    list_display = ('descripcion', 'usuario', 'docente',)
+
+@admin.register(ComentarioReservacionLugar)
+class AdminComentarioReservacionLugar(admin.ModelAdmin):
+    list_display = ('reservacionLugar', 'usuario',)
--- recursos/models.py
+++ recursos/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 from espaciofisico.models import Sede
+from academia.models import Grupo
+
 
 # Create your models here.
 
@@ -25,3 +28,62 @@ class Recursos(models.Model):
 
     def __unicode__(self):
         return self.descripcion
+
+
+class ReservacionLugar(models.Model):
+    descripcion = models.CharField(max_length=45, verbose_name = "Descripción",
+        help_text='Nombre de la reservación, máximo 45 caracteres')
+    fechaReserva = models.DateTimeField(auto_now_add = True,
+        verbose_name="Fecha de Reservación")
+    fechaInicio = models.DateTimeField(auto_now = False,
+        verbose_name="Fecha de Inicio")
+    fechaFinalizacion = models.DateTimeField(auto_now = False,
+        verbose_name = "Fecha de Finalización")
+    usuario = models.ForeignKey(User, verbose_name = "Usuario")
+    docente = models.ForeignKey(User, verbose_name = "Docente",
+        related_name="reservacionlugar_docente") #related_name permite diferenciar entre usuario y docente - recursion
+    funcionario = models.ForeignKey(User, verbose_name="Funcionario",
+        related_name="reservacionlugar_Funcionario",
+        blank = True, null = True,
+        help_text='Funcionario que da cierre a prestamo')
+    observacionSolicitud = models.TextField(max_length = 1000,
+        verbose_name = "Observación Solicitud", help_text='1.000 caracteres',
+        blank = True, null = True)
+    grupo = models.ForeignKey(Grupo, verbose_name = "Grupo")
+    estadoActivo = models.BooleanField(default=True)
+    Lugar = models.ForeignKey(Recursos, verbose_name = "Lugar")
+
+    def __unicode__(self):
+        return self.descripcion
+
+
+class ComentarioReservacionLugar(models.Model):
+    reservacionLugar = models.ForeignKey(ReservacionLugar,
+        verbose_name="Reservación de Lugar")
+    usuario = models.ForeignKey(User, verbose_name = "Usuario")
+    comentario = models.TextField(max_length = 500,
+        verbose_name = "Comentario", help_text='500 caracteres',
+        blank = True, null = True)
+    def __unicode__(self):
+        return self.usuario
+
+
+
+# class ReservacionRecursoTecnologico(models.Model):
+#     estadoActivo = models.BooleanField(default=True)
+#     reservacion = models.ForeignKey(ReservacionLugar,
+#         verbose_name="Reservación")
+#     recurso = models.ForeignKey(Recursos, verbose_name="Recurso")
+#     usuario = models.ForeignKey(User, verbose_name="Solicitante")
+#     funcionario = models.ForeignKey(User, verbose_name="Funcionario",
+#         related_name="reservacionrecursotecnologico_Funcionario",
+#         blank = True, null = True)
+#     observacionSolicitud = models.TextField(max_length = 1000,
+#         verbose_name = "Observación Solicitud", help_text='1.000 caracteres',
+#         blank = True, null = True)
+#     observacionEntrega = models.TextField(max_length = 1000,
+#         verbose_name = "Observación Entrega", help_text='1.000 caracteres',
+#         blank = True, null = True)
+#     observacionRecepcion = models.TextField(max_length=1000,
+#         verbose_name="Observación Recepción", help_text='1.000 caracteres',
+#         blank = True, null = True)
